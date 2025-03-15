13026e838fecc20cd8a36f50003d14f24987923e
MoneymanMunya <Nyamhinguramunya@gmail.com>
2025-03-15 15:21:25 +0000

    Saved all files and progress

--- app/database.py
+++ app/database.py
@@ -3,6 +3,18 @@ from fastapi import FastAPI
 app = FastAPI()
 
 @app.get("/")
-def read_root():
+def home():
     return {"message": "FastAPI is working!"}
+
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+DATABASE_URL = "postgresql://MoneymanMunya:Munya10*@localhost/omni_db"
+
+engine = create_engine(DATABASE_URL)  
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)  
+Base = declarative_base()
+
+
 
